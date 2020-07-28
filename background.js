b0209e384465c6314f03be946ef1b7ea8140240c
Glitch (hello-webpage) <none>
2020-07-28 03:21:16 +0100

    2

--- background.js
+++ background.js
@@ -1,8 +1,3 @@
-// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-//     alert('updated from background ');
-//     console.log("tabId = "+tabId+" changeInfo = "+changeInfo+" tab = "+tab);
-// });
-
 chrome.runtime.onInstalled.addListener(function() {
     // ...
   
@@ -10,19 +5,39 @@ chrome.runtime.onInstalled.addListener(function() {
       // changeInfo object: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/onUpdated#changeInfo
       // status is more reliable (in my case)
       // use "alert(JSON.stringify(changeInfo))" to check what's available and works in your case
-    //   if (changeInfo.status === 'complete') {
-    //     chrome.runtime.sendMessage("test");
-       
-            // chrome.tabs.executeScript({
-            //   file: 'onYouTube.js'
-            // });
-        // chrome.tabs.sendMessage(tabId, {
-        //   message: 'TabUpdated'
-        // });
-       // alert("new tab");
-    //    chrome.tabs.executeScript(tabId,{file:"onYouTube.js"},function(){
-    //     console.log("run when needed");
-    // });
+      if (changeInfo.status === 'complete') {
+         // alert("newtab");
+        chrome.tabs.sendMessage(tabId, {
+          message: 'TabUpdated'
+        });
       }
     })
   });
+
+// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+//     alert('updated from background ');
+//     console.log("tabId = "+tabId+" changeInfo = "+changeInfo+" tab = "+tab);
+// });
+
+// chrome.runtime.onInstalled.addListener(function() {
+//     // ...
+  
+//     // chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+//     //   // changeInfo object: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/onUpdated#changeInfo
+//     //   // status is more reliable (in my case)
+//     //   // use "alert(JSON.stringify(changeInfo))" to check what's available and works in your case
+//     // //   if (changeInfo.status === 'complete') {
+//     // //     chrome.runtime.sendMessage("test");
+       
+//     //         // chrome.tabs.executeScript({
+//     //         //   file: 'onYouTube.js'
+//     //         // });
+//     //     // chrome.tabs.sendMessage(tabId, {
+//     //     //   message: 'TabUpdated'
+//     //     // });
+//     //    // alert("new tab");
+//     // //    chrome.tabs.executeScript(tabId,{file:"onYouTube.js"},function(){
+//     // //     console.log("run when needed");
+//     // });
+//     //        });
+// //   });
--- onYouTube.js
+++ onYouTube.js
@@ -5,13 +5,66 @@ var hasEngSubs = false;
 
 var BTSChecked = false;
 var subtitlesChecked = false;
-console.log("on youtube ran");
+
+var currentUrl;
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.message === 'TabUpdated') {
+    if (window.location.toString().includes("watch")) {
+      reset();
+    };
+  }
+});
+
+function reset() {
+  isBTSVid = false;
+  hasEngSubs = false;
+  BTSChecked = false;
+  subtitlesChecked = false;
+  console.log("reset called");
+
+  if (window.location.toString() != currentUrl) {
+    console.log("on youtube ran");
+    currentUrl = window.location.toString();
+    // waitForElementToDisplay("#channel-name", 5000);
+    getSubtitlesData();
+  }
+}
+function getSubtitlesData() {
+  var url = window.location.toString();
+  var watchId;
+  var urlParts = url.split("youtube.com/watch?v=");
+  if (urlParts.length > 0) {
+    watchId = urlParts[1];
+
+    var request = new XMLHttpRequest();
+    request.open("GET", "https://video.google.com/timedtext?type=list&v=" + watchId);
+    request.send();
 
 
-if (window.location.toString().includes("watch")) {
-  waitForElementToDisplay("#channel-name", 5000);
-  getSubtitlesData();
-};
+    request.onload = function () {
+      var xml = request.responseXML;
+      var languageNodes = xml.childNodes[0].childNodes;
+      for (var i = 0; i < languageNodes.length; i++) {
+        var code = languageNodes[i].getAttribute("lang_code");
+        if (code === "en" || code === "en-gb" || code === "en-us") {
+          // console.log("this vid has eng subtitles");
+          hasEngSubs = true;
+
+        }
+      }
+      subtitlesChecked = true;
+     // bothProcessesFinished();
+     waitForElementToDisplay("#channel-name", 5000);
+    }
+  }
+}
+
+
+// if (window.location.toString().includes("watch")) {
+//   waitForElementToDisplay("#channel-name", 5000);
+//   getSubtitlesData();
+// };
 
 function waitForElementToDisplay(selector, time) {
   if (document.querySelector(selector) != null) {
@@ -44,44 +97,21 @@ function checkTitle(isBTSVid) {
   }
   console.log("isBTSVid = " + isBTSVid);
   BTSChecked = true;
-  bothProcessesFinished();
+  console.log("isBTSVid = " + isBTSVid);
+  bothProcessesFinished(isBTSVid);
 }
 
 
 
-function getSubtitlesData() {
-  var url = window.location.toString();
-  var watchId;
-  var urlParts = url.split("youtube.com/watch?v=");
-  if (urlParts.length > 0) {
-    watchId = urlParts[1];
-
-    var request = new XMLHttpRequest();
-    request.open("GET", "https://video.google.com/timedtext?type=list&v=" + watchId);
-    request.send();
 
 
-    request.onload = function () {
-      var xml = request.responseXML;
-      var languageNodes = xml.childNodes[0].childNodes;
-      for (var i = 0; i < languageNodes.length; i++) {
-        var code = languageNodes[i].getAttribute("lang_code");
-        if (code === "en" || code === "en-gb" || code === "en-us") {
-         // console.log("this vid has eng subtitles");
-          hasEngSubs = true;
-
-        }
-      }
-      subtitlesChecked = true;
-      bothProcessesFinished();
-    }
-  }
-}
-
-function bothProcessesFinished() {
+function bothProcessesFinished(isThisBTSVid) {
+  console.log("isBTSVid = " + isBTSVid);
+  console.log(" unchecked isBTSVid = " + isBTSVid + " hasEngSubs = " + hasEngSubs);
+  
   if (BTSChecked && subtitlesChecked) { //got both data
-    console.log("isBtsVid = " + isBTSVid + " hasEngSubs = " + hasEngSubs);
-    if (isBTSVid) {
+    console.log("isBTSVid = " + isBTSVid + " hasEngSubs = " + hasEngSubs);
+    if (isThisBTSVid) {
       if (hasEngSubs) {
         turnSubsOn();
       } else {
@@ -112,9 +142,9 @@ function searchForAltVid() {
       window.open(translated[i][1]);
       var playButton = document.getElementsByClassName('ytp-play-button')[0];
       var playing = playButton.getAttribute("aria-label") === "Pause (k)";
-     if(playing){
-      playButton.click();
-     }
+      if (playing) {
+        playButton.click();
+      }
       // window.location.replace(translated[i][1]);
       //add in user preferences to open in new tab or same tab
       break;
